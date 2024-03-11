2783acfc40b2bcad4a5310abaf54414c36fb0969
Dirk Michael <dirkmichaelnm@gmail.com>
2024-03-11 15:30:02 +0100

    Normals visualizer added

---
+++ Assets/Scripts/Terrain/Debug/NormalVisualizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Terrain.Debug
+{
+    public class NormalVisualizer : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            var meshFilter = GetComponent<MeshFilter>();
+            if (meshFilter != null)
+            {
+                var mesh = meshFilter.sharedMesh;
+                var vertices = mesh.vertices;
+                var normals = mesh.normals;
+
+                Gizmos.color = Color.cyan;
+                for (var i = 0; i < vertices.Length; i++)
+                {
+                    Gizmos.DrawLine(vertices[i], vertices[i] + normals[i].normalized * 0.02f);
+                }
+            }
+        }
+    }
+}
