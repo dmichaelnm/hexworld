703df56dbf7519f5e707a8ae2c310b45f805cb40
Dirk Michael <dirkmichaelnm@gmail.com>
2024-07-18 07:56:46 +0200

    Initial project setup.

---
+++ Source/HexWorld/HexWorld.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HexWorld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HexWorld, "HexWorld" );
